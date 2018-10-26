42d8549885c410f18a51194030c314e79953993b
LUCAS SECCHI <lsecchi@ar0fvfwr12rhv2j.ml.com>
2018-10-26 14:53:40 -0300

    se arregla funciones.h

--- Ejercicio4/funciones.h
+++ Ejercicio4/funciones.h
@@ -1,11 +1,6 @@
-#ifndef _FUNCIONES_H
-#define _FUNCIONES_H
-
 #include<stdlib.h> 
 
 int menu();
 void cargar_nota();
 void consultar_promedio_general();
 void consultar_promedio_por_materia();
-
-#endif
